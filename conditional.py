84ea91caae5b44c13d68eaecd3ab5cca4c1899eb
mavoo-a11y <marvinlone456@gmail.com>
2025-05-05 21:01:03 +0300

    conditional.py

---
+++ conditional.py
@@ -0,0 +1,9 @@
+Marvin_weight = 60.0
+normal_weight = 50.0
+
+if Marvin_weight < normal_weight:
+    print("Marvin is underweight")
+elif Marvin_weight == normal_weight:
+    print("Marvin is normal")
+else:
+    print("Marvin is overweight")
