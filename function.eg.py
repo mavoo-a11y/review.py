60c96bb8565dafc6a82f2daad1e5cb365aa65335
mavoo-a11y <marvinlone456@gmail.com>
2025-05-10 20:44:43 +0300

    function.eg.py

---
+++ function.eg.py
@@ -0,0 +1,12 @@
+def happy_birthday(name):
+    print(F'Happy Birthday to {name}!')
+    print("You are now getting older!")
+    print("Have it a blast!")
+    print("Wish you the best in life!")
+    print("May you have a long life with Prosperity!")
+    print()
+
+
+happy_birthday("Mavo")
+    
+
